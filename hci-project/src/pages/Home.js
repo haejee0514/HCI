96e6d9f749def6bff713b499b1298024728a1926
haejee0514 <haejee0514@naver.com>
2024-11-19 12:54:17 +0900

    Fix Home logo text

--- hci-project/src/pages/Home.js
+++ hci-project/src/pages/Home.js
@@ -1,23 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../styles/Home.css'; // 경로 수정
 
 function Home() {
   const navigate = useNavigate();
+  const [isClicked, setIsClicked] = useState(false); // 클릭 상태 관리
 
   const handleClick = () => {
-    navigate('/NameInput');
+    setIsClicked(true); // 클릭 상태 변경
+    setTimeout(() => {
+      navigate('/NameInput'); // 클릭 후 페이지 이동
+    }, 200); // 색상 변경 후 약간의 딜레이를 추가 (선택사항)
   };
 
   return (
     <div className="home-container">
       <img src="/img/home.png" alt="배경 이미지" className="background-image" />
-      <img
-        src="/img/NeuroCanvas.png"
-        alt="NeuroCanvas"
-        className="center-image"
+      <div
+        className={`logo-text ${isClicked ? 'clicked' : ''}`} // 클릭 시 클래스 추가
         onClick={handleClick}
-      />
+      >
+        NeuroCanvas
+      </div>
     </div>
   );
 }
